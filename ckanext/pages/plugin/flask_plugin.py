dace3a38a071f9c94cb3967940cd30bae7b92f0f
M. Uzair Afzal <studies9990@gmail.com>
2022-01-24 23:45:56 +0500

    Links for add new page & blog and edit current page & blog fixed

--- ckanext/pages/plugin/flask_plugin.py
+++ ckanext/pages/plugin/flask_plugin.py
@@ -1,18 +1,39 @@
 # -*- coding: utf-8 -*-
 
 import ckan.plugins as p
+import ckan.plugins.toolkit as toolkit
 
 from ckanext.pages.blueprint import pages as pages_blueprint
 import ckanext.pages.cli as cli
+
+def get_recent_blog_posts(number = 5, exclude = None):
+    blog_list = toolkit.get_action('ckanext_pages_list')(
+        None, {'order_publish_date': True, 'private': False, 'page_type': 'blog'}
+    )
+    new_list = []
+    for blog in blog_list:
+        if exclude and blog['name'] == exclude:
+            continue
+        new_list.append(blog)
+        if len(new_list) == number:
+            break
+
+    return new_list
 
 
 class MixinPlugin(p.SingletonPlugin):
 
     p.implements(p.IBlueprint)
     p.implements(p.IClick)
+    p.implements(p.ITemplateHelpers)
 
     def get_blueprint(self):
         return [pages_blueprint]
 
     def get_commands(self):
         return cli.get_commands()
+
+    def get_helpers(self):
+        return {
+            'get_recent_blog_posts': get_recent_blog_posts
+        }
